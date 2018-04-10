d6d7bfd981e1718f6dbc9b10813f1ba63927ef19
X.Heart <xiaoxin.wang@xinhuajindian.com>
2018-04-10 10:14:27 +0800

    1

--- config.js
+++ config.js
@@ -3,14 +3,15 @@
  */
 
 var serverUrl = 'https://xheart.leanapp.cn/server'
-var baseUrl = "http://www.xinhuajindian.com/xhjd-web"
-var apiHost = "http://www.xinhuajindian.com/huoqService2.0/"
+var HOST = 'http://www.xinhuajindian.com'
+var baseUrl = `${HOST}/xhjd-web`
+var apiHost = `${HOST}/huoqService2.0`
 
 var config = {
   baseUrl,
   serverUrl,
   // 我要理财接口
-  manageUrl: `${apiHost}/productList`,
+  manageUrl: `${HOST}/productList`,
   // manageUrl: 'https://xheart.leanapp.cn/server',
   lessonUrl: `${apiHost}/Product/newMore`,
   runreportsUrl: `${apiHost}/OperatingReport/getReportData`,
