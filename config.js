54dc022de2a6dd5e8f3deee98ad96667cfbbd108
X.Heart <xiaoxin.wang@xinhuajindian.com>
2018-08-03 14:16:37 +0800

    修复图片显示

--- config.js
+++ config.js
@@ -4,17 +4,18 @@
 
 var serverUrl = 'https://xheart.leanapp.cn/server'
 var HOST = 'http://www.xinhuajindian.com'
-var baseUrl = `${HOST}/xhjd-web`
+var baseUrl = HOST
 var apiHost = `${HOST}/huoqService2.0`
+var iosHost = 'http://ios.xinhuajindian.com/huoqService2.0'
 
 var config = {
   baseUrl,
   serverUrl,
   // 我要理财接口
-  manageUrl: `${HOST}/productList`,
+  manageUrl: 'http://47.98.189.203:8080/v3/100901/100901001',
   // manageUrl: 'https://xheart.leanapp.cn/server',
-  lessonUrl: `${apiHost}/Product/newMore`,
-  runreportsUrl: `${apiHost}/OperatingReport/getReportData`,
+  lessonUrl: `${iosHost}/Product/newMore`,
+  runreportsUrl: `${iosHost}/OperatingReport/getReportData`,
 };
 
 module.exports = config
--- pages/runreports-detail/runreports-detail.js
+++ pages/runreports-detail/runreports-detail.js
@@ -1,20 +1,26 @@
 // pages/runreports-detail.js
 import { getDetail } from '../../api/detail.js';
-
 Page({
   data: {
-    nodes: ''
+    nodes: '',
+    imgUrl: ""
   },
   onLoad: function (options) {
     var that = this;
     var Url = unescape(options.url)
-    var baseUrl = Url.substr(0, Url.lastIndexOf("/") + 1)
-    getDetail(Url).then((res) => {
-      var article = /<body>([\s\S]*?)<\/body>/gi.exec(res)[1];
-      article = article.replace(/src="/g, 'class="rich-image" src="' + baseUrl);
-      that.setData({
-        nodes: article
-      })
+    var baseUrl = Url.substr(0, Url.indexOf(".com") + 4)
+    var index = Url.substr(Url.lastIndexOf("=") + 1)
+    console.log(baseUrl)
+    that.setData({
+      imgUrl: `${baseUrl}/xhjd-h5/static/images/month-report/month_report${index}.png`
     })
+    // getDetail(Url).then((res) => {
+    //   var article = /<body>([\s\S]*?)<\/body>/gi.exec(res)[1];
+    //   article = article.replace(/src="/g, 'class="rich-image" src="' + baseUrl);
+    //   console.log(article)
+    //   that.setData({
+    //     nodes: article
+    //   })
+    // })
   }
 })
